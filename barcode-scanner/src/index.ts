2c659682e5fe12e120afaf61d8a2d3f34a18c7f5
Matěj Kohútek <matej.kohutek@gmail.com>
2019-09-26 15:52:06 +0200

    Change incoming data format parsing in index.ts

--- barcode-scanner/src/index.ts
+++ barcode-scanner/src/index.ts
@@ -4,21 +4,17 @@ import { IncomingMessage, ServerResponse } from "http";
 import { send, buffer } from "micro";
 import  * as statusCode from "http-status-codes";
 import { Decoder } from "./decode";
-import { fileSync } from "tmp";
-import { writeFileSync } from "fs";
 
 const decode_handler: AugmentedRequestHandler = async function (req: IncomingMessage, res: ServerResponse) {
+    const data = JSON.parse(await buffer(req) as string);
     let result;
-    let readers = req.headers.readers;
+    let readers = data.readers;
     if (typeof readers === "string") {
         readers = [readers];
     } else if (readers === undefined) {
-        send(res, statusCode.BAD_REQUEST, "No header - reader - defined.");
-        return;
+        readers = ["code_128_reader"];
     }
-    const tempFilePath = fileSync({postfix:""+req.headers.name}).name;
-    writeFileSync(tempFilePath, await buffer(req));
-    result = await Decoder.decode(tempFilePath, readers);
+    result = await Decoder.decode(data.image, readers);
     if (result.result) {
         result.result = result.result.codeResult.code;
     }
@@ -27,4 +23,7 @@ const decode_handler: AugmentedRequestHandler = async function (req: IncomingMes
 
 module.exports = router(
     post("/decode", decode_handler),
-    get("/info", (req: IncomingMessage, res: ServerResponse) => send(res, statusCode.OK, "Test info")));
+    get("/info", (req: IncomingMessage, res: ServerResponse) => {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        send(res, statusCode.OK, "Test info");
+    }));
